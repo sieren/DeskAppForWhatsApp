e4a7f8fb96e1d219b63c333443f384864d8d8317
Matt Sieren <matthias@s-r-n.de>
2016-01-03 19:08:40 +0100

    Update Build File for Mac
    Manually copy Sparkle.Framework into the App Bundle.

--- src/build_project.sh
+++ src/build_project.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ./clean.sh
+cp -rf ./lib/Sparkle.framework ~/Library/Frameworks
 ~/Qt/5.5/clang_64/bin/qmake .
 make
+~/Qt/5.5/clang_64/bin/macdeployqt DeskAppForWhatsApp.app -verbose=2
+cp -Rf ./lib/Sparkle.framework DeskAppForWhatsApp.app/Contents/Frameworks
 ~/Qt/5.5/clang_64/bin/macdeployqt DeskAppForWhatsApp.app -dmg -verbose=2 
